4a1fb694ab23e10c00ad09b03088f6aa49983c33
Musa Nadeem <musan3661@gmail.com>
2026-04-09 12:36:38 +0500

    added fontset and added comments for Chip8.hpp

--- include/Chip8.hpp
+++ include/Chip8.hpp
@@ -5,27 +5,70 @@
 #include <string>
 
 
+const unsigned int FONTSET_SIZE = 80;
+const uint8_t FONTSET[80] = {
+0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+0x20, 0x60, 0x20, 0x20, 0x70, // 1
+0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 class Chip8{
 
+
     public:
-    Chip8();
+    Chip8(){
+        //standard PC Start Address for CHIP8
+        this->PC = 0x200;
+
+        //Clearing Memory
+        for (int i = 0; i < 4095; i++)
+        {
+            this->memory[i] = 0;
+            
+        }
+
+        //Clearing Registers
+        for (int i = 0; i < 15; i++)
+        {
+            this->V[i] = 0;
+        }
+        
+        
+    };
+
+    // Loading the game
     bool LoadRom(const std::string& filename);
-    void Cycle();
     
-    uint32_t display[64*32]{};
-    uint8_t keypad[16]{};
-    uint8_t soundTimer{};
-    uint8_t delayTimer{};
+    // Fetch-Decode-Execute Cycle
+    void Cycle();
+
+
+    uint32_t display[64*32]{}; // 64 x 32 pixel display
+    uint8_t keypad[16]{}; // Keypad mapped to [0 to F]
+    uint8_t soundTimer{}; // Sound timer ticking at 60Hz
+    uint8_t delayTimer{}; // Delay timer ticking at 60Hz
 
     private:
-    uint8_t V[16]{};
-    uint16_t stack[16]{};
-    uint8_t memory[4096]{};
-    uint8_t sp{};
-    uint16_t I{};
-    uint16_t PC{};
-    uint16_t opcode{}; 
+    uint8_t V[16]{}; // 16 regular use registers with a flag register VF
+    uint16_t stack[16]{}; // Stacks for storing memory addresses during subroutines
+    uint8_t memory[4096]{}; // 4KB memory
+    uint8_t sp{}; // Stack pointer (memory pointer for stack)
+    uint16_t I{}; // Index Register
+    uint16_t PC{}; // Program Counter
+    uint16_t opcode{}; // Stores the opcode
 
 };
 #endif
